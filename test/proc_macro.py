6f7ddfddd80cd03378599e5cda7781bb75e0fec3
David McNamee <david@mcnamee.io>
2024-12-12 02:46:24 -0500

    initial commit

---
+++ test/proc_macro.py
@@ -0,0 +1,17 @@
+from .__macros__ import my_func
+import re
+from macros.proc_macro import macro
+
+@macro()
+def my_macro(code: str) -> str:
+    res = code.replace("+", "-")
+    res = re.sub(r"return (.*)", r"return str(\1)", res)
+    res = res.replace("-> int", "-> str")
+    return res
+
+@my_macro(generated=my_func)
+def my_func(a: int, b: int) -> int:
+    return a + b
+
+print(type(my_func(1, 2)), my_func(1, 2))
+# prints "<class 'str'> -1"
