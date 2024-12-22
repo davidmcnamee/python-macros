fd87e7f67d7cf4aa544a838f407d9639dd8d4fdd
David McNamee <david@mcnamee.io>
2024-12-22 12:21:36 -0500

    make parsing and codegen more robust w/ ast

--- justfile
+++ justfile
@@ -4,6 +4,9 @@ set dotenv-load := true
 test:
     uv run python -m tests.proc_macro
 
+itest:
+    watchexec -e py -i '*/__macros__/*' just test
+
 publish:
     rm -rf *.egg-info dist
     uv build
--- tests/proc_macro.py
+++ tests/proc_macro.py
@@ -2,10 +2,10 @@ import re
 
 from mozzarella import macro
 
-from .__macros__ import my_func, my_other_func
+from .__macros__.types import GenStuff, gen_my_func, gen_my_other_func
 
 
-@macro()
+@macro
 def my_macro(code: str, *, x: int) -> str:
     print(x)
     res = code.replace("+", "-")
@@ -14,15 +14,38 @@ def my_macro(code: str, *, x: int) -> str:
     return res
 
 
-@my_macro(generated=my_other_func, x=5)
+@my_macro(gen=gen_my_other_func, x=5)
 def my_other_func(a: int, b: int) -> int:
     return a + 3 + b
 
 
-@my_macro(generated=my_func, x=4)
+@my_macro(gen=gen_my_func, x=6)
 def my_func(a: int, b: int) -> int:
     return a + b
 
 
 assert my_func(1, 2) == "-1"
 assert my_other_func(1, 2) == "-4"
+
+
+@my_macro(gen=GenStuff, x=7)
+class Stuff:
+    def __init__(self, c: int):
+        self.c = c
+
+    def my_method(self, a: int, b: int) -> int:
+        return a + b + self.c
+
+    @property
+    def whatever(self) -> int:
+        self.my_method(1, 2)
+        return 4
+
+
+class NewClass(Stuff):
+    def whatever2(self):
+        a = self.my_method(1, 2)
+        print(a)
+
+
+assert Stuff(5).my_method(3, 4) == str(3 - 4 - 5)
